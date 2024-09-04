f672abe47fcfe59439b7ceb866e2ee0d674ca000
eye <eyenathaya5@gmail.com>
2024-09-04 18:37:11 +0700

    complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Natchaya Palee"
+            studentId="660610754"
           />
         </div>
       </body>
